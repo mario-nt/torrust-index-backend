b330a722cc3b1cdd7692a4bd2e61b1967e8ea3f6
Mario <mariomnt945@gmail.com>
2024-06-27 01:15:44 +0200

    wip

--- src/app.rs
+++ src/app.rs
@@ -17,7 +17,7 @@ use crate::services::torrent::{
     DbTorrentListingGenerator, DbTorrentRepository, DbTorrentTagRepository,
 };
 use crate::services::user::{self, DbBannedUserList, DbUserProfileRepository, DbUserRepository, Repository};
-use crate::services::{authorization, proxy, settings, torrent};
+use crate::services::{about, authorization, proxy, settings, torrent};
 use crate::tracker::statistics_importer::StatisticsImporter;
 use crate::web::api::server::signals::Halted;
 use crate::web::api::server::v1::auth::Authentication;
@@ -137,6 +137,7 @@ pub async fn run(configuration: Configuration, api_version: &Version) -> Running
         user_profile_repository.clone(),
         user_authentication_repository.clone(),
     ));
+    let about_service = Arc::new(about::Service::new());
 
     // Build app container
 
@@ -171,6 +172,7 @@ pub async fn run(configuration: Configuration, api_version: &Version) -> Running
         registration_service,
         profile_service,
         ban_service,
+        about_service,
     ));
 
     // Start cronjob to import tracker torrent data and updating
